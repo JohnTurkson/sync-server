aaac9a46ca28132d32e4eb8d09537f27a8e0f4f3
JohnTurkson <git@johnturkson.com>
2022-03-05 12:44:41 -0800

    Update handler interface

--- handlers/src/main/kotlin/com/johnturkson/sync/handlers/definitions/LambdaHandler.kt
+++ handlers/src/main/kotlin/com/johnturkson/sync/handlers/definitions/LambdaHandler.kt
@@ -1,9 +1,9 @@
-package com.johnturkson.sync.handlers.functions
+package com.johnturkson.sync.handlers.definitions
 
 interface LambdaHandler<T, R> {
-    suspend fun processRequest(body: String): R
+    suspend fun processRequest(body: String?): R
     
-    fun decodeRequest(body: String): T?
+    fun decodeRequest(body: String?): T?
     
     fun encodeResponse(response: R): String
 }
