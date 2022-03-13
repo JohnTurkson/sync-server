12a8cac998b278a8b4ea9a7b98a2e9c490f4a5a4
JohnTurkson <git@johnturkson.com>
2022-03-12 23:51:44 -0800

    Add test table

--- infrastructure/src/main/kotlin/com/johnturkson/sync/infrastructure/SyncStack.kt
+++ infrastructure/src/main/kotlin/com/johnturkson/sync/infrastructure/SyncStack.kt
@@ -3,6 +3,13 @@ package com.johnturkson.sync.infrastructure
 import software.amazon.awscdk.Duration
 import software.amazon.awscdk.Stack
 import software.amazon.awscdk.StackProps
+import software.amazon.awscdk.services.dynamodb.Attribute
+import software.amazon.awscdk.services.dynamodb.AttributeType
+import software.amazon.awscdk.services.dynamodb.BillingMode
+import software.amazon.awscdk.services.dynamodb.GlobalSecondaryIndexProps
+import software.amazon.awscdk.services.dynamodb.LocalSecondaryIndexProps
+import software.amazon.awscdk.services.dynamodb.ProjectionType
+import software.amazon.awscdk.services.dynamodb.Table
 import software.amazon.awscdk.services.lambda.Code
 import software.amazon.awscdk.services.lambda.Function
 import software.amazon.awscdk.services.lambda.Runtime
@@ -10,6 +17,27 @@ import software.constructs.Construct
 
 class SyncStack(parent: Construct, name: String, props: StackProps? = null) : Stack(parent, name, props) {
     init {
+        val table = Table.Builder.create(this, "TestTable2")
+            .billingMode(BillingMode.PAY_PER_REQUEST)
+            .partitionKey(Attribute.builder().type(AttributeType.STRING).name("primary").build())
+            .sortKey(Attribute.builder().type(AttributeType.STRING).name("secondary").build())
+            .build()
+        
+        val lsi = LocalSecondaryIndexProps.builder()
+            .indexName("lsi")
+            .sortKey(Attribute.builder().type(AttributeType.STRING).name("secondary").build())
+            .projectionType(ProjectionType.ALL)
+            .build()
+        
+        val gsi = GlobalSecondaryIndexProps.builder().indexName("gsi")
+            .partitionKey(Attribute.builder().type(AttributeType.STRING).name("primary").build())
+            .sortKey(Attribute.builder().type(AttributeType.STRING).name("secondary").build())
+            .projectionType(ProjectionType.ALL)
+            .build()
+        
+        table.addGlobalSecondaryIndex(gsi)
+        table.addLocalSecondaryIndex(lsi)
+        
         Function.Builder.create(this, "TestFunction2")
             .description("test")
             .code(Code.fromAsset("../handlers/build/lambda/image/handlers.zip"))
