34521b440e94dca76824a3b150c93772fa198e8a
JohnTurkson <git@johnturkson.com>
2022-03-13 20:08:54 -0700

    Delete test table on `cdk destroy`

--- cdk/src/main/kotlin/com/johnturkson/sync/cdk/SyncStack.kt
+++ cdk/src/main/kotlin/com/johnturkson/sync/cdk/SyncStack.kt
@@ -1,6 +1,7 @@
 package com.johnturkson.sync.cdk
 
 import com.johnturkson.sync.functions.generated.Functions
+import software.amazon.awscdk.RemovalPolicy
 import software.amazon.awscdk.Stack
 import software.amazon.awscdk.services.dynamodb.Attribute
 import software.amazon.awscdk.services.dynamodb.AttributeType
@@ -18,6 +19,7 @@ class SyncStack(parent: Construct, name: String) : Stack(parent, name) {
             .billingMode(BillingMode.PAY_PER_REQUEST)
             .partitionKey(Attribute.builder().type(AttributeType.STRING).name("primary").build())
             .sortKey(Attribute.builder().type(AttributeType.STRING).name("secondary").build())
+            .removalPolicy(RemovalPolicy.DESTROY)
             .build()
         
         val lsi = LocalSecondaryIndexProps.builder()
