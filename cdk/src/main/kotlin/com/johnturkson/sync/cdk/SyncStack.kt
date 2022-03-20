c73b76d6ad885a318d3ceb5f12ac76f5f135d0a0
JohnTurkson <git@johnturkson.com>
2022-03-20 09:07:07 -0700

    Add test route

--- cdk/src/main/kotlin/com/johnturkson/sync/cdk/SyncStack.kt
+++ cdk/src/main/kotlin/com/johnturkson/sync/cdk/SyncStack.kt
@@ -3,6 +3,20 @@ package com.johnturkson.sync.cdk
 import com.johnturkson.sync.common.generated.Tables
 import com.johnturkson.sync.functions.generated.Functions
 import software.amazon.awscdk.Stack
+import software.amazon.awscdk.services.apigatewayv2.alpha.AddRoutesOptions
+import software.amazon.awscdk.services.apigatewayv2.alpha.DomainMappingOptions
+import software.amazon.awscdk.services.apigatewayv2.alpha.DomainName
+import software.amazon.awscdk.services.apigatewayv2.alpha.HttpApi
+import software.amazon.awscdk.services.apigatewayv2.alpha.HttpApiProps
+import software.amazon.awscdk.services.apigatewayv2.alpha.HttpMethod
+import software.amazon.awscdk.services.apigatewayv2.integrations.alpha.HttpLambdaIntegration
+import software.amazon.awscdk.services.certificatemanager.DnsValidatedCertificate
+import software.amazon.awscdk.services.certificatemanager.DnsValidatedCertificateProps
+import software.amazon.awscdk.services.route53.ARecord
+import software.amazon.awscdk.services.route53.HostedZone
+import software.amazon.awscdk.services.route53.HostedZoneProviderProps
+import software.amazon.awscdk.services.route53.RecordTarget
+import software.amazon.awscdk.services.route53.targets.ApiGatewayv2DomainProperties
 import software.constructs.Construct
 
 class SyncStack(parent: Construct, name: String) : Stack(parent, name) {
@@ -15,5 +29,54 @@ class SyncStack(parent: Construct, name: String) : Stack(parent, name) {
                 table.grantReadWriteData(function)
             }
         }
+        
+        val hostedZone = HostedZone.fromLookup(
+            this,
+            "HostedZone",
+            HostedZoneProviderProps.builder()
+                .domainName("johnturkson.com")
+                .build()
+        )
+        
+        val certificate = DnsValidatedCertificate(
+            this,
+            "Certificate",
+            DnsValidatedCertificateProps.builder()
+                .domainName("johnturkson.com")
+                .hostedZone(hostedZone)
+                .build()
+        )
+        
+        val domainName = DomainName.Builder.create(this, "DomainName")
+            .domainName("johnturkson.com")
+            .certificate(certificate)
+            .build()
+        
+        val apiDomainProperties = ApiGatewayv2DomainProperties(
+            domainName.regionalDomainName,
+            domainName.regionalHostedZoneId
+        )
+        
+        val aRecord = ARecord.Builder.create(this, "ARecord")
+            .zone(hostedZone)
+            .target(RecordTarget.fromAlias(apiDomainProperties))
+            .build()
+        
+        val api = HttpApi(this, "Api", HttpApiProps.builder()
+            .disableExecuteApiEndpoint(true)
+            .defaultDomainMapping(DomainMappingOptions.builder()
+                .domainName(domainName)
+                .build())
+            .build())
+        
+        val routes = functions.map { function ->
+            AddRoutesOptions.builder()
+                .path("/${function.functionName}")
+                .methods(listOf(HttpMethod.GET))
+                .integration(HttpLambdaIntegration("${function.functionName}LambdaIntegration", function))
+                .build()
+        }
+        
+        routes.forEach { route -> api.addRoutes(route) }
     }
 }
