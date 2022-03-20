ab53f83747483a477ab6bc5342187af96ac7e229
JohnTurkson <git@johnturkson.com>
2022-03-20 10:40:58 -0700

    Update Function creation defaults

--- generators/src/main/kotlin/com/johnturkson/sync/generators/annotations/lambda/Function.kt
+++ generators/src/main/kotlin/com/johnturkson/sync/generators/annotations/lambda/Function.kt
@@ -1,3 +1,3 @@
 package com.johnturkson.sync.generators.annotations.lambda
 
-annotation class Function(val timeout: Int = 3, val memory: Int = 128)
+annotation class Function(val timeout: Int = 1, val memory: Int = 1024)
