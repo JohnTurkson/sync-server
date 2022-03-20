4981de71f0e9b189f7c05ee4d4f750e814715605
JohnTurkson <git@johnturkson.com>
2022-03-20 15:26:51 -0700

    Update table permissions

--- cdk/src/main/kotlin/com/johnturkson/sync/cdk/SyncStack.kt
+++ cdk/src/main/kotlin/com/johnturkson/sync/cdk/SyncStack.kt
@@ -31,7 +31,7 @@ class SyncStack(
         
         tables.forEach { table ->
             functions.forEach { function ->
-                table.grantReadData(function)
+                table.grantReadWriteData(function)
             }
         }
         
